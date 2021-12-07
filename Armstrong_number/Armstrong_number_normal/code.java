6af3a72244e1eb8b052f4743b7b854c0d5b5c846
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-07 22:20:17 +0530

    Update code.java

--- Armstrong_number/Armstrong_number_normal/code.java
+++ Armstrong_number/Armstrong_number_normal/code.java
@@ -1,1 +1,26 @@
-
+import java.util.*;
+class armstrong
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int num,temp,sum=0,rem;
+        System.out.println("Enter number for checking for armstrong :");
+        num=in.nextInt();
+        temp=num;
+         while(temp!=0)
+        {
+            rem=temp%10;
+            sum=sum+(rem*rem*rem);
+            temp=temp/10;
+        }
+        if(num==sum)
+        {
+        System.out.println("Armstrong number.");
+        }
+        else
+        {
+        System.out.println("Not Armstrong Number..");
+        }
+    }
+}
