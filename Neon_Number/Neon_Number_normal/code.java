215dca314e089f1f3e877fbb421f2b3932720fc0
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-07 22:29:53 +0530

    Update code.java

--- Neon_Number/Neon_Number_normal/code.java
+++ Neon_Number/Neon_Number_normal/code.java
@@ -4,7 +4,7 @@ class neon
     public static void main(String args[])
     {
         Scanner in=new Scanner(System.in);
-        int num,temp,sum=0,rem,square=0;;
+        int num,sum=0,rem,square=0;
         System.out.println("Enter number for checking for Neon Number :");
         num=in.nextInt();
         square=num*num;
