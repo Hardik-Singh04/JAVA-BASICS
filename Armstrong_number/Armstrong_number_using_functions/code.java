0be785695f43ae32bc52a9bb7820d6b503b1be2d
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-07 22:18:19 +0530

    Update code.java

--- Armstrong_number/Armstrong_number_using_functions/code.java
+++ Armstrong_number/Armstrong_number_using_functions/code.java
@@ -1,1 +1,33 @@
-
+import java.util.*;
+class armstrong
+{
+    int armstrong(int num)
+    {
+        int temp,sum=0,rem;
+        temp=num;
+         while(temp!=0)
+        {
+            rem=temp%10;
+            sum=sum+(rem*rem*rem);
+            temp=temp/10;
+        }
+        return sum;
+    }
+    public static void main(String args[])
+    {
+        armstrong obj=new armstrong();
+        Scanner in=new Scanner(System.in);
+        int num,sum;
+        System.out.println("Enter number for checking for armstrong :");
+        num=in.nextInt();
+        sum=obj.armstrong(num);
+        if(num==sum)
+        {
+        System.out.println("Armstrong number.");
+        }
+        else
+        {
+        System.out.println("Not Armstrong Number..");
+        }
+    }
+}
