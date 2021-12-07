663764dfcac537c8a362f86e02f0aeca72d65b76
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-07 22:26:28 +0530

    Update code.java

--- Neon_Number/Neon_Number_normal/code.java
+++ Neon_Number/Neon_Number_normal/code.java
@@ -1,1 +1,26 @@
-
+import java.util.*;
+class neon
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int num,temp,sum=0,rem,square=0;;
+        System.out.println("Enter number for checking for Neon Number :");
+        num=in.nextInt();
+        square=num*num;
+         while(square!=0)
+        {
+            rem=square%10;
+            sum=sum+rem;
+            square=square/10;
+        }
+        if(num==sum)
+        {
+        System.out.println("Neon Number.");
+        }
+        else
+        {
+        System.out.println("Not Neon Number..");
+        }
+    }
+}
