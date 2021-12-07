741581f78dec81afe418f95d49e775021eb54434
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-07 21:50:52 +0530

    Update code.java

--- multiplication_of_two_numbers_using_functions/code.java
+++ multiplication_of_two_numbers_using_functions/code.java
@@ -1,1 +1,22 @@
-
+import java.util.*;
+class multiply
+{
+    int multiply(int a,int b)
+    {
+        int multiply=0;
+        multiply=a*b;
+        return multiply;
+    }
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        multiply obj=new multiply();
+        int multiply=0;
+        int a,b;
+        System.out.println("Enter two number for multiplication :");
+        a=in.nextInt();
+        b=in.nextInt();
+        multiply=obj.multiply(a,b);
+        System.out.println("Value of multiplication is :"+multiply);
+    }
+}
