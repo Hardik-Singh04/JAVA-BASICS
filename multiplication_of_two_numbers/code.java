c4bec987d02c6ddb4cb9362f92ad064393b9a597
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-07 21:52:13 +0530

    Create code.java

---
+++ multiplication_of_two_numbers/code.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class multiply
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int multiply=0;
+        int a,b;
+        System.out.println("Enter two number for multiply :");
+        a=in.nextInt();
+        b=in.nextInt();
+        multiply=a*b;
+        System.out.println("Value of multiplication is :"+multiply);
+    }
+}
