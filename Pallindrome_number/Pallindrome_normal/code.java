0512bf18856896f3115731922d9e30d5a530c16b
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-07 22:08:18 +0530

    Update code.java

--- Pallindrome_number/Pallindrome_normal/code.java
+++ Pallindrome_number/Pallindrome_normal/code.java
@@ -1,1 +1,26 @@
-
+import java.util.*;
+class pallindrome
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int num,temp,sum=0,rem;
+        System.out.println("Enter number for checking for pallindrome :");
+        num=in.nextInt();
+        temp=num;
+        while(temp!=0)
+        {
+            rem=temp%10;
+            sum=sum*10+rem;
+            temp=temp/10;
+        }
+        if(num==sum)
+        {
+        System.out.println("Pallindrome number.");
+        }
+        else
+        {
+        System.out.println("Not Pallindrome Number..");
+        }
+    }
+}
