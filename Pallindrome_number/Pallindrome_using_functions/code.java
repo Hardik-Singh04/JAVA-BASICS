d28a6d7559471a43528b3294819b2ddb7e91e24b
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-07 22:12:09 +0530

    Update code.java

--- Pallindrome_number/Pallindrome_using_functions/code.java
+++ Pallindrome_number/Pallindrome_using_functions/code.java
@@ -1,1 +1,33 @@
-
+import java.util.*;
+class pallindrome
+{
+    int pallindrome(int num)
+    {
+        int temp,sum=0,rem;
+        temp=num;
+         while(temp!=0)
+        {
+            rem=temp%10;
+            sum=sum*10+rem;
+            temp=temp/10;
+        }
+        return sum;
+    }
+    public static void main(String args[])
+    {
+        pallindrome obj=new pallindrome();
+        Scanner in=new Scanner(System.in);
+        int num,sum;
+        System.out.println("Enter number for checking for pallindrome :");
+        num=in.nextInt();
+        sum=obj.pallindrome(num);
+        if(num==sum)
+        {
+        System.out.println("Pallindrome number.");
+        }
+        else
+        {
+        System.out.println("Not Pallindrome Number..");
+        }
+    }
+}
