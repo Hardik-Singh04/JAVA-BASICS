0aff24882d363969b10634f2d8461e5cf1b986ab
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-08 10:11:32 +0530

    Create code.java

---
+++ Happy_Number/Happy_Number_using_Function/code.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class happyfun
+{
+    int check(int num)
+    {
+        int sum=0,rem;
+        do
+        {
+            sum=0;
+            while(num!=0)
+            {
+            rem=num%10;
+            sum=sum+(rem*rem);
+            num=num/10;
+            }
+            num=sum;
+        }while(sum>6);
+        return sum;
+    }
+    public static void main(String args[])
+    {
+        happyfun obj=new happyfun();
+        Scanner in=new Scanner(System.in);
+        int num,sum=0;;
+        System.out.println("Enter number for checking for Happy Number :");
+        num=in.nextInt();
+        sum=obj.check(num);
+        if(sum==1)
+        {
+        System.out.println("Happy Number.");
+        }
+        else
+        {
+        System.out.println("Not Happy Number..");
+        }
+    }
+}
