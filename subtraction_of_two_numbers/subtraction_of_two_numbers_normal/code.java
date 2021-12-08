f4537abd99a98ea7c4bfef7bc4141d2f5e581d83
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-08 11:08:06 +0530

    Create code.java

---
+++ subtraction_of_two_numbers/subtraction_of_two_numbers_normal/code.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class subtract
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int subtract=0;
+        int a,b;
+        System.out.println("Enter two number for subtaction :");
+        a=in.nextInt();
+        b=in.nextInt();
+        subtract=a-b;
+        System.out.println("Value of subtract is :"+subtract);
+        
+    }
+}
