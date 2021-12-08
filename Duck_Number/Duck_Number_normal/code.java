4da5cba376e7481b63738323e90ec99bc80eefea
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-08 10:06:22 +0530

    Create code.java

---
+++ Duck_Number/Duck_Number_normal/code.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class duck
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int num,rem,count=0;
+        System.out.println("Enter number to check for Duck Number :");
+        num=in.nextInt();
+        while(num>0)
+        {
+            rem=num%10;
+            if(rem==0)
+            {
+                count=1;
+            }
+            num=num/10;
+        }
+        
+        if(count==1)
+        {
+            System.out.println("Duck Number.");
+        }
+        else
+        {
+            System.out.println("Not Duck Number.");
+        }
+    }
+}
