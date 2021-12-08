87aa392f2b71fab579412af559ec9c402becc6a6
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-08 10:13:43 +0530

    Update code.java

--- Happy_Number/Happy_Number_Normal/code.java
+++ Happy_Number/Happy_Number_Normal/code.java
@@ -1,1 +1,32 @@
-
+import java.util.*;
+class happyfun
+{
+    public static void main(String args[])
+    {
+        
+        Scanner in=new Scanner(System.in);
+        int num,sum=0,rem;
+        System.out.println("Enter number for checking for Happy Number :");
+        num=in.nextInt();
+         
+        do
+        {
+            sum=0;
+            while(num!=0)
+            {
+            rem=num%10;
+            sum=sum+(rem*rem);
+            num=num/10;
+            }
+            num=sum;
+        }while(sum>6);
+        if(sum==1)
+        {
+        System.out.println("Happy Number.");
+        }
+        else
+        {
+        System.out.println("Not Happy Number..");
+        }
+    }
+}
