1c96751cc249387805bcc52e0a7d0d0a2701e2a6
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-08 10:04:30 +0530

    Create code.java

---
+++ Factorial/Factorial_Number/code.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class factorial
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int num,fact=1,i;
+        System.out.println("Enter number whose factorial is to be found :");
+        num=in.nextInt();
+        for(i=1;i<=num;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println("Factorial is :"+fact);
+       
+    }
+}
