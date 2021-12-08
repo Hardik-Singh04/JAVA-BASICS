c19ce3256da7844e2589bea063d04d742ae1d0d7
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-08 10:07:43 +0530

    Create code.java

---
+++ Automorphic_Number/Automorphic_Number_Normal/code.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class automorphic
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int num,sq=0,last=0;
+        System.out.println("Enter number to check for automorphic :");
+        num=in.nextInt();
+        sq=num*num;
+        num=num%10;
+        last=sq%10;
+        if(num==last)
+        {
+            System.out.println("Automorphic Number.");
+        }
+        else
+        {
+            System.out.println("Not Automorphic Number.");
+        }
+    }
+}
