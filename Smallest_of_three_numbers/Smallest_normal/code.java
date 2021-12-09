194e2ab5ebc5210d6396f6118123025cb479a006
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-09 19:14:38 +0530

    Update code.java

--- Smallest_of_three_numbers/Smallest_normal/code.java
+++ Smallest_of_three_numbers/Smallest_normal/code.java
@@ -1,1 +1,26 @@
-
+import java.util.*;
+class smallest
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int a,b,c,small;
+        System.out.println("Enter three numbers :");
+        a=in.nextInt();
+        b=in.nextInt();
+        c=in.nextInt();
+        if(a<b&&a<c)
+        {
+            small=a;
+        }
+        if(b<c&&b<a)
+        {
+            small=b;
+        }
+        else
+        {
+            small=c;
+        }
+        System.out.println("Small : "+small);
+    }
+}
