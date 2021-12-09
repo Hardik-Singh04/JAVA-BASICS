c0b696e68c476745ff140eb6d0a8a486dfc5cea1
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-09 18:59:48 +0530

    Create code.java

---
+++ Largest_of_three_numbers/Largest_normal/code.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class largest
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int a,b,c,max;
+        System.out.println("Enter three numbers :");
+        a=in.nextInt();
+        b=in.nextInt();
+        c=in.nextInt();
+        if(a>b&&a>c)
+        {
+            max=a;
+        }
+        if(b>c&&b>a)
+        {
+            max=b;
+        }
+        else
+        {
+            max=c;
+        }
+        System.out.println("Max : "+max);
+    }
+}
