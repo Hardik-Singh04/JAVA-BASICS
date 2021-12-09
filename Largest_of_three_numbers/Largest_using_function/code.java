90c06e02ecee0e2b0147023358596a8dc814d288
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-09 19:02:14 +0530

    Create code.java

---
+++ Largest_of_three_numbers/Largest_using_function/code.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class largestfun
+{
+    int largest(int a,int b,int c)
+    {
+        int max;
+         if(a>b&&a>c)
+        {
+            max=a;
+        }
+        if(b>c&&b>a)
+        {
+            max=b;
+        }
+        else
+        {
+            max=c;
+        }
+        return max;
+    }
+    public static void main(String args[])
+    {
+        largestfun obj=new largestfun();
+        Scanner in=new Scanner(System.in);
+        int a,b,c,max;
+        System.out.println("Enter three numbers :");
+        a=in.nextInt();
+        b=in.nextInt();
+        c=in.nextInt();
+        max=obj.largest(a,b,c);
+        System.out.println("Max : "+max);
+    }
+}
