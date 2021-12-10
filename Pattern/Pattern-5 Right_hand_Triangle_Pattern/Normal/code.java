59f7c0f9c4e735f2a0c8c05b309f4e8b7d078889
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-10 09:51:21 +0530

    Create code.java

---
+++ Pattern/Pattern-5 Right_hand_Triangle_Pattern/Normal/code.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class pattern
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int num;
+        System.out.println("Enter number of rows :");
+        num=in.nextInt();  
+        for(int i=0;i<num;i++)
+        {
+            for(int j=0;j<=num;j++)
+            {
+                 if((i+j)>=num)
+                {
+                     System.out.print("*");
+                }
+                 else
+                {
+                     System.out.print(" ");
+                }
+            }
+        System.out.println();
+        }
+    }
+}
+
