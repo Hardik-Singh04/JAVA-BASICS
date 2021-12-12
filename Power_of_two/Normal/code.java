8004e56e71a18ceb58cc07cb982e9be696ef564a
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2021-12-12 21:35:52 +0530

    Update code.java

--- Power_of_two/Normal/code.java
+++ Power_of_two/Normal/code.java
@@ -1,1 +1,27 @@
-
+import java.util.*;
+class calculatefun
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int num;
+        System.out.println("Enter the number :");
+        num=in.nextInt();
+        while(num!=1)
+        {
+            if(num%2==0)
+            {
+                num=num/2;
+            }
+            else
+            {
+                System.out.println("Not power of 2");
+                break;
+            }
+        }
+        if(num==1)
+        {
+            System.out.println("Power of 2");
+        }
+    }
+}
