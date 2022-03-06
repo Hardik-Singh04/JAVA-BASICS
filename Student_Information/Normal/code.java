46929ef82b2868cc04f1f5d5109225548fd579b2
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-06 21:29:44 +0530

    Update code.java

--- Student_Information/Normal/code.java
+++ Student_Information/Normal/code.java
@@ -1,1 +1,18 @@
-.
+class Student
+{
+    public static void main(String args[])
+    {
+	int roll;
+	String name,university,branch;
+	
+	roll=1;
+	name="Hardik";
+	university="GEHU";
+	branch="CSE";
+	
+	System.out.println("Name :"+name);
+	System.out.println("Roll Number:"+roll);
+	System.out.println("University:"+university);
+	System.out.println("Branch:"+branch);	
+    }
+}
