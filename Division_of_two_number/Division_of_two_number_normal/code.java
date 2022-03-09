1f0058cd65549da9dd3b10c82b3c1e69d6ebfac0
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-09 16:32:21 +0530

    Create code.java

---
+++ Division_of_two_number/Division_of_two_number_normal/code.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class divide
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int divide=0;
+        int a,b;
+        System.out.println("Enter two number for division :");
+        a=in.nextInt();
+        b=in.nextInt();
+        divide=a/b;
+        System.out.println("Value of division is :"+divide);
+    }
+}
