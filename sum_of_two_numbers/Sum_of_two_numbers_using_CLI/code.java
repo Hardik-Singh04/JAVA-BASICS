c247b7edacfd9e2cb0d42b33b1674d1cd951debe
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-09 16:21:52 +0530

    Update code.java

--- sum_of_two_numbers/Sum_of_two_numbers_using_CLI/code.java
+++ sum_of_two_numbers/Sum_of_two_numbers_using_CLI/code.java
@@ -1,1 +1,9 @@
-.
+class sum
+{
+  public static void main(String args[])
+  {
+    int x=Integer.parseInt(args[0]);
+    int y=Integer.parseInt(args[1]);
+    System.out.println("Sum is : "+(x+y));
+  }
+}
