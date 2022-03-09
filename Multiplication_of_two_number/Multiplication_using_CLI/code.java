d4f60a1be1a6ed9b0216f7ed16659ed296f83954
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-09 16:28:00 +0530

    Create code.java

---
+++ Multiplication_of_two_number/Multiplication_using_CLI/code.java
@@ -0,0 +1,10 @@
+class Multiply
+{
+  public static void main(String args[])
+  {
+    int x=Integer.parseInt(args[0]);
+    int y=Integer.parseInt(args[1]);
+   
+      System.out.println("Multiply is : "+(x*y));
+   }
+}
