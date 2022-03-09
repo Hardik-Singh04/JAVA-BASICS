9bf310b44378047b5b35e789a05eff1b92f910b3
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-03-09 16:33:47 +0530

    Create code.java

---
+++ Division_of_two_number/Division_of_two_number_using_CLI/code.java
@@ -0,0 +1,10 @@
+class Division
+{
+  public static void main(String args[])
+  {
+    int x=Integer.parseInt(args[0]);
+    int y=Integer.parseInt(args[1]);
+   
+      System.out.println("Division of x/y is : "+(x/y));
+   }
+}
